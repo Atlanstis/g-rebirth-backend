cc19c31099d8aa6c544dac22fa2dacc8c1b2c378
Gypsophila <futur_wxc@163.com>
2024-10-08 10:59:14 +0800

    feat(基础): 增加返回数据的自动包装，通过 @RawData 进行判断是否对数据自动包装

---
+++ src/core/interceptor/transform.interceptor.ts
@@ -0,0 +1,27 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable, map } from 'rxjs';
+import { ResponseData } from 'src/classes';
+import { IS_RAW_DATA } from 'src/core';
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+  constructor(private reflector: Reflector) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const isRawData = this.reflector.getAllAndOverride<boolean>(IS_RAW_DATA, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    return next.handle().pipe(
+      map((data) => {
+        return isRawData ? data : ResponseData.success(data);
+      }),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupInterceptor } from './core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 注册拦截器
+  setupInterceptor(app);
+
   await app.listen(3000);
 }
+
 bootstrap();
